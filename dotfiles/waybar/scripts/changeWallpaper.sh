9d9d3a6ba842eecc013b505df8ca939c585ce3a4
CaueMiyazaki <caue.eyti@gmail.com>
2026-04-03 14:57:12 -0300

    feat: initial niri setup

--- dotfiles/waybar/scripts/changeWallpaper.sh
+++ dotfiles/waybar/scripts/changeWallpaper.sh
@@ -14,11 +14,11 @@ refreshRate="72"
 wallpaperDir="$(xdg-user-dir PICTURES)/Wallpapers"
 selectedImage="$(find "$wallpaperDir" -name '*.png' | shuf -n1)"
 
-if pgrep "swww-daemon" > /dev/null ; then
+if pgrep "awww-daemon" > /dev/null ; then
 	if [[ "$transition_type" == "grow" ]]; then
-		swww img "$selectedImage" --transition-fps $refreshRate --transition-type $transition_type --transition-duration 1 --transition-pos 1725,20 --invert-y
+		awww img "$selectedImage" --transition-fps $refreshRate --transition-type $transition_type --transition-duration 1 --transition-pos 1725,20 --invert-y
 	else
-		swww img "$selectedImage" --transition-fps $refreshRate --transition-type $transition_type --transition-duration 1
+		awww img "$selectedImage" --transition-fps $refreshRate --transition-type $transition_type --transition-duration 1
 	fi
 else
   notify-send "Error" "Wallpaper daemon not running!"
