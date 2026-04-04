3beb9fa20b99d0519881491e87e03d67e9d9edc3
CaueMiyazaki <caue.eyti@gmail.com>
2026-04-04 19:22:28 -0300

    refactor(fuzzel): changing some configs and how fuzzel opens through different methods e.g. keybind or waybar script

--- dotfiles/waybar/scripts/runPowermenu.sh
+++ dotfiles/waybar/scripts/runPowermenu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# If its being called from waybar it will have a top-right anchor
+# if no argument was passed, then CHOICE is set to zero
+# This :- is bashs "Default value" operator
+CHOICE=${1:-0}
+
 # Options
 lock=" Lock"
 suspend="󰤄 Suspend"
@@ -8,7 +13,11 @@ reboot=" Reboot"
 shutdown=" Shutdown"
 
 runFuzzel() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown\n" | fuzzel --dmenu -l 5
+	if [ "$CHOICE" == 1 ]; then
+		echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown\n" | fuzzel --dmenu -l 5 --anchor top-right --x-margin 7 --y-margin 7 --hide-prompt
+	else
+		echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown\n" | fuzzel --dmenu -l 5 --hide-prompt
+	fi
 }
 
 chosen="$(runFuzzel)"
